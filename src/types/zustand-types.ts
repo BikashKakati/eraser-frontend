3c05b6eb9583d877f357edc25ec3ccd6c69b406d
Bikash Kakati <bikashkakati796@gmail.com>
2026-03-01 12:17:17 +0530

    [ADD] Properly manage the arrow creation

--- src/types/zustand-types.ts
+++ src/types/zustand-types.ts
@@ -1,7 +1,24 @@
+import { Position } from '@xyflow/react';
 
 export type Tool = 'select' | 'rectangle' | 'circle' | 'arrow' | 'pan_zoom';
 
 export type ActiveToolStoreType = {
   activeTool: Tool;
   setActiveTool: (tool: Tool) => void;
+  drawingArrowFrom: { 
+    x: number, 
+    y: number,
+    parentId?: string,
+    relativeX?: number,
+    relativeY?: number,
+    handlePosition?: Position
+  } | null;
+  setDrawingArrowFrom: (pos: { 
+    x: number, 
+    y: number,
+    parentId?: string,
+    relativeX?: number,
+    relativeY?: number,
+    handlePosition?: Position
+  } | null) => void;
 };
