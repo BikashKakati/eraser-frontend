f3383e3f179dc97ceb07690b35e565b47fcad567
Bikash Kakati <bikashkakati796@gmail.com>
2026-03-29 10:42:49 +0530

    [FIX] Multiple active nodes and edges at same time issues

--- src/store/actions/interaction-actions.ts
+++ src/store/actions/interaction-actions.ts
@@ -1,11 +1,23 @@
-import type { Tool } from '../../types/zustand-types';
 import type { Position } from '@xyflow/react';
 import type { EditorStoreType } from '../../types/store-types';
+import type { Tool } from '../../types/zustand-types';
+import { sidebarTools } from '../../constant';
 
 export const setActiveToolAction = (
-  _state: EditorStoreType,
+  state: EditorStoreType,
   tool: Tool
 ): Partial<EditorStoreType> => {
+  if (tool !== sidebarTools.SELECT) {
+    const newNodes = state.nodes.map(node => ({ ...node, selected: false }));
+    const newEdges = state.edges.map(edge => ({ ...edge, selected: false }));
+    return { 
+      activeTool: tool, 
+      nodes: newNodes, 
+      edges: newEdges,
+      selectedNodeIds: [],
+      selectedEdgeIds: []
+    };
+  }
   return { activeTool: tool };
 };
 
