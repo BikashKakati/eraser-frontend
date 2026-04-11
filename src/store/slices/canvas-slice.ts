c56408151f4376c033703831354616be334a7105
Bikash Kakati <bikashkakati796@gmail.com>
2026-04-11 23:49:37 +0530

    [UPDATE] Committing every action using other  slices actions

--- src/store/slices/canvas-slice.ts
+++ src/store/slices/canvas-slice.ts
@@ -13,19 +13,37 @@ import {
   ungroupAction,
 } from '../actions/canvas-actions';
 
-export const createCanvasSlice: StateCreator<EditorStoreType, [], [], CanvasSlice> = (set, _get) => ({
+export const createCanvasSlice: StateCreator<EditorStoreType, [], [], CanvasSlice> = (set, get) => ({
   nodes: [],
   edges: [],
   selectedNodeIds: [],
   selectedEdgeIds: [],
   initializeCanvasData: (nodes, edges) => set((state: EditorStoreType) => initializeCanvasDataAction(state, nodes, edges)),
-  updateShapeNode: (id, updates) => set((state: EditorStoreType) => updateShapeNodeAction(state, id, updates)),
-  updateEdgeStyle: (id, updates) => set((state: EditorStoreType) => updateEdgeStyleAction(state, id, updates)),
-  deleteElements: (ids, type) => set((state: EditorStoreType) => deleteElementsAction(state, ids, type)),
+  updateShapeNode: (id, updates) => {
+    get().commitHistory();
+    set((state: EditorStoreType) => updateShapeNodeAction(state, id, updates));
+  },
+  updateEdgeStyle: (id, updates) => {
+    get().commitHistory();
+    set((state: EditorStoreType) => updateEdgeStyleAction(state, id, updates));
+  },
+  deleteElements: (ids, type) => {
+    get().commitHistory();
+    set((state: EditorStoreType) => deleteElementsAction(state, ids, type));
+  },
   onNodesChange: (changes) => set((state: EditorStoreType) => onNodesChangeAction(state, changes)),
   onEdgesChange: (changes) => set((state: EditorStoreType) => onEdgesChangeAction(state, changes)),
-  onConnect: (connection) => set((state: EditorStoreType) => onConnectAction(state, connection)),
+  onConnect: (connection) => {
+    get().commitHistory();
+    set((state: EditorStoreType) => onConnectAction(state, connection));
+  },
   updateAnchorPositions: (changes, anchorChanges) => set((state: EditorStoreType) => updateAnchorPositionsAction(state, changes, anchorChanges) || {}),
-  createGroup: () => set((state: EditorStoreType) => createGroupAction(state)),
-  ungroup: () => set((state: EditorStoreType) => ungroupAction(state)),
+  createGroup: () => {
+    get().commitHistory();
+    set((state: EditorStoreType) => createGroupAction(state));
+  },
+  ungroup: () => {
+    get().commitHistory();
+    set((state: EditorStoreType) => ungroupAction(state));
+  },
 });
--- src/store/slices/interaction-slice.ts
+++ src/store/slices/interaction-slice.ts
@@ -1,24 +1,30 @@
 import type { EditorStoreType, InteractionSlice } from '../../types/store-types';
 import { setActiveToolAction, setDrawingArrowFromAction } from '../actions/interaction-actions';
-import { 
-  initializeArrowFromExternalAction, 
-  startFreehandDrawAction, 
-  updateFreehandDrawAction, 
-  finalizeFreehandDrawAction 
+import {
+  initializeArrowFromExternalAction,
+  startFreehandDrawAction,
+  updateFreehandDrawAction,
+  finalizeFreehandDrawAction
 } from '../actions/editor-actions';
 import type { StateCreator } from 'zustand';
 import { sidebarTools } from '../../constant';
 
-export const createInteractionSlice: StateCreator<EditorStoreType, [], [], InteractionSlice> = (set, _get) => ({
+export const createInteractionSlice: StateCreator<EditorStoreType, [], [], InteractionSlice> = (set, get) => ({
   activeTool: sidebarTools.SELECT,
   drawingArrowFrom: null,
   anchorNodeDetails: null,
   drawingShapeDetails: null,
-  
+
   setActiveTool: (tool) => set((state: EditorStoreType) => setActiveToolAction(state, tool)),
   setDrawingArrowFrom: (pos) => set((state: EditorStoreType) => setDrawingArrowFromAction(state, pos)),
-  initializeArrowFromExternal: () => set((state: EditorStoreType) => initializeArrowFromExternalAction(state) || {}),
-  startFreehandDraw: (pos) => set((state: EditorStoreType) => startFreehandDrawAction(state, pos) || {}),
+  initializeArrowFromExternal: () => {
+    get().commitHistory();
+    set((state: EditorStoreType) => initializeArrowFromExternalAction(state) || {});
+  },
+  startFreehandDraw: (pos) => {
+    get().commitHistory();
+    set((state: EditorStoreType) => startFreehandDrawAction(state, pos) || {});
+  },
   updateFreehandDraw: (pos) => set((state: EditorStoreType) => updateFreehandDrawAction(state, pos) || {}),
   finalizeFreehandDraw: () => set((state: EditorStoreType) => finalizeFreehandDrawAction(state) || {}),
 });
