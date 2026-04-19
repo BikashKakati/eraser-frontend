142b0ddecde3698c0072d353b4d630ea61b583a8
Bikash Kakati <bikashkakati796@gmail.com>
2026-04-19 16:55:28 +0530

    [UPDATE] Set a auth:logout event listener to listen api response where we have to logout

--- src/FlowbitApp.tsx
+++ src/FlowbitApp.tsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/Router";
+import { useAuthStore } from "./store/auth-store";
 
 const FlowbitApp: React.FC = () => {
+  const logout = useAuthStore(state => state.logout);
+
+  useEffect(() => {
+    const handleLogout = () => {
+      logout();
+      window.location.href = '/login';
+    };
+    window.addEventListener('auth:logout', handleLogout);
+    return () => window.removeEventListener('auth:logout', handleLogout);
+  }, [logout]);
+
   return (
     <RouterProvider router={router} />
   );
